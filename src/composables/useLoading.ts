e2f91c45621f2aac27e2162df75aaf65749ea1c0
jmj <wnalsals1127@gmail.com>
2025-07-18 13:56:17 +0900

    refactor: composables & change useModal with route

--- src/composables/useLoading.ts
+++ src/composables/useLoading.ts
@@ -2,17 +2,17 @@ import { shallowRef } from 'vue'
 
 const isLoading = shallowRef(false)
 
-const showLoading = () => {
-  isLoading.value = true
-  document.body.classList.add('no-scroll')
-}
-
-const hideLoading = () => {
-  isLoading.value = false
-  document.body.classList.remove('no-scroll')
-}
-
 export const useLoading = () => {
+  const showLoading = () => {
+    isLoading.value = true
+    document.body.classList.add('no-scroll')
+  }
+
+  const hideLoading = () => {
+    isLoading.value = false
+    document.body.classList.remove('no-scroll')
+  }
+
   return {
     isLoading,
     showLoading,
--- src/composables/useModal.ts
+++ src/composables/useModal.ts
@@ -1,20 +1,40 @@
-import { shallowRef } from 'vue'
+import { shallowRef, watch } from 'vue'
 import type { Component } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
 const isOpen = shallowRef(false)
 const modalComponent = shallowRef<Component | null>(null)
 
-const open = (component: Component) => {
-  modalComponent.value = component
-  isOpen.value = true
-}
-
-const close = () => {
-  isOpen.value = false
-  modalComponent.value = null
-}
-
 export const useModal = () => {
+  const router = useRouter()
+  const route = useRoute()
+
+  watch(
+    () => route.query.modal,
+    (modalQuery) => {
+      if (!modalQuery && isOpen.value) {
+        setClose()
+      }
+    },
+  )
+
+  const setClose = () => {
+    isOpen.value = false
+    modalComponent.value = null
+    document.body.classList.remove('no-scroll')
+  }
+
+  const open = (component: Component) => {
+    isOpen.value = true
+    modalComponent.value = component
+    document.body.classList.add('no-scroll')
+    router.push({ query: { ...route.query, modal: 'open' } })
+  }
+
+  const close = () => {
+    router.back()
+  }
+
   return {
     isOpen,
     modalComponent,
